82bd2cf8b2f70845700cb5dad7f56123db821f97
Ammar sahyoun <ammarzgr@hotmail.com>
2022-01-10 22:35:19 +0100

    create basket shopping package

---
+++ TDD_shopping_basket/test/shoping_basket_test.py
@@ -0,0 +1,26 @@
+import unittest
+from item import Item
+from shopping_basket import Basket
+
+
+
+class ShopingBasketTest(unittest.TestCase):
+    def test_empty_basket_total(self):
+        basket = Basket([])
+        self.assertEqual(basket.total(), 0)
+
+    def test_single_item_quantity(self):
+        basket = Basket([Item(100.0, 1)])
+        self.assertEqual(basket.total(), 100.0)
+
+    def test_two_items_quantity_one(self):
+        basket = Basket([Item(100.0, 1), Item(100.0, 1)])
+        self.assertEqual(basket.total(), 200.0)
+
+    def test_single_item_quantity_two(self):
+        basket = Basket([Item(100.0, 2)])
+        self.assertEqual(basket.total(), 200.0)
+
+
+if __name__ == "__main__":
+    pass
